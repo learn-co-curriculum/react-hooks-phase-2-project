7e5b12183ace34fa5fe69ac015d00c2dadfdc142
Zoe Guidry <oyezoey@gmail.com>
2023-04-26 09:49:03 -0500

    nyt api

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -1,6 +1,6 @@
 import {useState, useEffect} from 'react'
 
-const apiKey = `4b7a803e73094ba69d4de8c272c6798f`
+const apiKey = `M8hsBtQcuiZFM4pD0mGiDAa0UMbA0UaQ`
 let country = 'US'
 
 
@@ -9,13 +9,13 @@ function Home(){
     const [topHeadlines, setTopHeadlines] = useState([])
 
 
-  const baseUrl = `https://newsapi.org/v2/`
-  const newsUrl = baseUrl + `top-headlines?country=${country}&apiKey=${apiKey}`
+    const baseUrl = `https://api.nytimes.com/svc/topstories/v2/`
+    const newsUrl = baseUrl + `home.json?api-key=${apiKey}`
 
   const fetchTopHeadlines = () => {
       fetch(newsUrl)
       .then(res => res.json())
-      .then(headlinesData=>setTopHeadlines(headlinesData.articles))
+      .then(headlinesData=>setTopHeadlines(headlinesData.results))
       .catch(error=>console.log(error))
 
    }
@@ -32,10 +32,10 @@ function Home(){
            <div style={{ paddingTop: 20 , paddingBottom: 20}}>
            <h3 style={{textAlign: 'center', textJustify: 'center'}}>{article.title}</h3>
            <figure>
-           <img src={article.urlToImage} alt={article.description}  style={{ display: 'block', width: 850 , height: 550, marginLeft: 'auto', marginRight: 'auto'}} />
-           <figcaption style={{textAlign: 'center', textJustify: 'center', fontSize:'smaller'}}>{article.description}</figcaption>
+           <img src={article.multimedia[0].url} alt={article.multimedia[0].caption}  style={{ display: 'block', width: 850 , height: 550, marginLeft: 'auto', marginRight: 'auto'}} />
+           <figcaption style={{textAlign: 'center', textJustify: 'center', fontSize:'smaller'}}>{article.abstract}</figcaption>
            </figure>
-           <pre style={{textAlign: 'center', textJustify: 'center', fontSize: 'medium'}}>{article.content}</pre>
+           {/* <pre style={{textAlign: 'center', textJustify: 'center', fontSize: 'medium'}}>{article.content}</pre> */}
            </div>)
           
   
